99cf7c18ddd0f16ec70e18ac565a78f99c6b0485
direcciones507 <ventas@direcionesdigitalespty.net>
2026-03-15 19:32:16 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,13 +12,6 @@ import {
   orderBy
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAnrizkZp2rGhpYYyek3v49Q6Qs_ylEvbw",
